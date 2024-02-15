c75b2de0d9e945c39a820538532528929b4fecdc
henri-kapidani <97576915+henri-kapidani@users.noreply.github.com>
2024-02-15 21:15:08 +0400

    Add project files.

---
+++ Concessionario/Index.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="Concessionario.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Prezzo: <span id="LblTotal" runat="server"></span></h2>
+            <ul id="LstSelectedOptions" runat="server">
+            </ul>
+
+            <asp:DropDownList ID="DrpWarranty" runat="server" AutoPostBack="True">
+                <asp:ListItem ID="Year1" Value="1" Text="1 anno"></asp:ListItem>
+                <asp:ListItem ID="Year2" Value="2" Text="2 anni"></asp:ListItem>
+                <asp:ListItem ID="Year3" Value="3" Text="3 anni"></asp:ListItem>
+            </asp:DropDownList>
+
+            <asp:CheckBoxList ID="LstOptions" runat="server" AutoPostBack="True">
+                <asp:ListItem ID="Option1" Value="100" Text="Option 1"></asp:ListItem>
+                <asp:ListItem ID="Option2" Value="100" Text="Option 2"></asp:ListItem>
+                <asp:ListItem ID="Option3" Value="200" Text="Option 3"></asp:ListItem>
+                <asp:ListItem ID="Option4" Value="300" Text="Option 4"></asp:ListItem>
+            </asp:CheckBoxList>
+
+            <asp:DropDownList ID="DrpCars" runat="server" AutoPostBack="True"></asp:DropDownList>
+            <img id="ImgCar" runat="server" width="150" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Concessionario/Index.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Concessionario
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int Total = 0;
+
+            string connectionString = ConfigurationManager.ConnectionStrings["DbConcessionariaConnectionString"].ToString();
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            conn.Open();
+            SqlCommand sqlCommand = new SqlCommand("SELECT * FROM Cars", conn);
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while(reader.Read())
+                {
+                    if (!IsPostBack)
+                    {
+                        ListItem listItem = new ListItem();
+                        listItem.Value = reader["Id"].ToString();
+                        listItem.Text = reader["Model"].ToString();
+                        DrpCars.Items.Add(listItem);
+                    }
+                    
+                    if (reader["Id"].ToString() == DrpCars.SelectedValue)
+                    {
+                        ImgCar.Src = reader["image"].ToString();
+                        Total += Convert.ToInt32(reader["Price"]);
+                    }
+                }
+            }
+
+            Total += Convert.ToInt32(DrpWarranty.SelectedValue) * 120;
+
+            string selectedOptions = "";
+            foreach (ListItem item in LstOptions.Items)
+            {
+                if (item.Selected)
+                {
+                    Total += Convert.ToInt32(item.Value);
+                    selectedOptions += $"<li>{item.Text} - {item.Value}</li>";
+                }
+            }
+            LblTotal.InnerText = Total.ToString();
+            LstSelectedOptions.InnerHtml = selectedOptions;
+        }
+    }
+}
